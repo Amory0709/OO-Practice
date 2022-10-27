e757aa490da495f86206856d1c9c85e11d2f56cf
Mengyu <mhan8@slb.com>
2022-10-27 11:26:13 +0800

    feat: add speed up

---
+++ OOPractice/Car.cs
@@ -0,0 +1,19 @@
+﻿namespace OOPractice
+{
+    using System;
+    public class Car
+    {
+        private string name;
+        private string speed;
+        public Car(string name, string speed)
+        {
+            this.name = name;
+            this.speed = speed;
+        }
+
+        public string SpeedUp()
+        {
+            return this.name + ": speed up " + this.speed + "km/h";
+        }
+    }
+}
